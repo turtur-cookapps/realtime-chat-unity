947aee1c59b8a09c2f546c5c72729080b21349b3
turtur <yjko@cookapps.com>
2018-09-11 16:21:31 +0900

    firebase 설치이후 필수 폴더만 이동

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
 
 public class Main : MonoBehaviour {
 
+	private DatabaseReference db;
 	// Use this for initialization
 	void Start () {
-		
+		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://realtime-chat-unity.firebaseio.com/");
+		this.db = FirebaseDatabase.DefaultInstance.RootReference;
 	}
 	
 	// Update is called once per frame
